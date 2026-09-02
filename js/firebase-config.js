ce78cc324d355b3791b52014cf52aeebbaee5b35
ahmednabil9988770-coder <ahmednabil9988770@gmail.com>
2026-09-02 22:15:52 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,15 +1,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
+
 const firebaseConfig = {
-apiKey: "AIzaSyAMFwMsOfykKRek82HQsdae9czwIxtEZWg",
-authDomain: "new-nova-future.firebaseapp.com",
-projectId: "new-nova-future",
-storageBucket: "new-nova-future.firebasestorage.app",
-messagingSenderId: "101150557975",
-appId: "1:101150557975:web:ac0786d4238bb30ab11952"
+    apiKey: "AIzaSyAMFwMsOfykKRek82HQsdae9czwIxtEZWg",
+    authDomain: "new-nova-future.firebaseapp.com",
+    projectId: "new-nova-future",
+    storageBucket: "new-nova-future.firebasestorage.app",
+    messagingSenderId: "101150557975",
+    appId: "1:101150557975:web:ac0786d4238bb30ab11952"
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
-export { app, auth };
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
