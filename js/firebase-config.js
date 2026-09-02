032e71bc78ec94a8a69f22237f7f0095d21684d4
ahmednabil9988770-coder <ahmednabil9988770@gmail.com>
2026-09-02 21:27:13 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
 
+const firebaseConfig = {
+apiKey: "AIzaSyAMFwMsOfykKRek82HQsdae9czwIxtEZWg",
+authDomain: "new-nova-future.firebaseapp.com",
+projectId: "new-nova-future",
+storageBucket: "new-nova-future.firebasestorage.app",
+messagingSenderId: "101150557975",
+appId: "1:101150557975:web:ac0786d4238bb30ab11952"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
